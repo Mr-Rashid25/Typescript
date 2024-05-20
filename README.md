705b880e976cf8ab0676343459d37d74daad21d5
Rashid <160320931+Mr-Rashid25@users.noreply.github.com>
2024-05-20 18:45:21 +0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Typescript
+Word Counter Command-Line Program in TypeScript
+NPM : rashid25-word-counter-project
+This TypeScript program is a command-line utility that counts the number of words in a user-provided sentence. It utilizes the inquirer library to prompt the user for input and processes the input to determine the word count. The program is designed to be simple and user-friendly, making it easy to quickly determine the word count of any sentence.
---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquier from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.cyanBright("\n \t \t Wellcome to Rashid's - Word Counter\n"));
+const answer:{
+  sentence:string
+} = await inquier.prompt([
+  {
+    name:"sentence",
+    type:"input",
+    message:"Enter your sentence to count the words."
+  }
+])
+// Triming the sentence and spiliting it into the words.
+const words = answer.sentence.trim().split(" ");
+console.log(chalk.bold.yellowBright("-> Sentence words:"));
+console.log(words);
+console.log(chalk.bold.magentaBright(`\nYou have (${chalk.bold.red(words.length)}) words in your sentence.`));
+console.log('>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<');
